23152439974f1a39e5556fc76fd9cd442df4b6d0
Aurojit Panda <apanda@cs.berkeley.edu>
2013-10-27 11:34:11 -0700

    Web proxy and nicer rules

--- model/simple-python/wproxy.py
+++ model/simple-python/wproxy.py
@@ -5,7 +5,7 @@ This is in Python since OCaml wasn't working
 import z3
 packet = z3.Datatype('Packet')
 address = z3.DeclareSort('Address')
-endhost, (a, b, c, d, fw_eh) = z3.EnumSort('Endhost', ['a', 'b', 'c', 'd', 'fw_eh'])
+endhost, (a, b, c, d, fw_eh, proxy) = z3.EnumSort('Endhost', ['a', 'b', 'c', 'd', 'fw_eh', 'proxy'])
 packet.declare('packet', ('src', address), ('dest', address), ('origin', endhost))
 packet = packet.create()
 address_in_host = z3.Function('hostHasAddr', endhost, address, z3.BoolSort ())
@@ -33,22 +33,30 @@ def GetBaseConditions ():
     
     # All received packets were once sent (don't invent packets).
     base_conditions.append(z3.ForAll([eh1, eh2, p], z3.Implies(recv(eh1, eh2, p),
-                                     z3.Exists([eh3], send(packet.origin(p), eh3, p)))))
+                                     z3.Exists([eh3], send(address_to_host(packet.src(p)), eh3, p)))))
+    # Turn off loopback, loopback makes me sad
+    base_conditions.append(z3.ForAll([eh1, eh2, p], z3.Implies(send(eh1, eh2, p), eh1 != eh2)))
+    base_conditions.append(z3.ForAll([eh1, eh2, p], z3.Implies(recv(eh1, eh2, p), eh1 != eh2)))
     #base_conditions.append(z3.ForAll([eh1,eh3, eh4, p], z3.Implies((eh1 == packet.origin(p)) and\
     #                         recv(eh3, eh4, p),
     #                        z3.Exists([eh2], send(eh1, eh2, p))))) 
     return base_conditions
 
-def FirewallDenyRules (solver, fw, hosts, rules):
+def FirewallDenyRules (solver, fw, adj, rules):
     p = z3.Const('p', packet)
     eh = z3.Const('temp_eh', endhost)
+    eh2 = z3.Const('temp_eh2', endhost)
+    solver.add(z3.ForAll([eh, p], z3.Implies(send(fw, eh, p), z3.Exists([eh2], recv(eh2, fw, p)))))
+    adjacency_constraint = z3.Or(map(lambda n: eh == n, adj))
+    # This is just about connectivity
+    solver.add(z3.ForAll([eh, p], z3.Implies(recv(eh, fw, p),\
+                            adjacency_constraint)))
+    solver.add(z3.ForAll([eh, p], z3.Implies(send(fw, eh, p),\
+                            adjacency_constraint)))
 
-    # Coerce all host traffic through a firewall
-    for host in hosts:
-        solver.add(z3.ForAll([eh, p], z3.Implies(send(host, eh, p),\
-                                    eh == fw)))
     if len(rules) == 0:
         return
+    # The firewall never invents packets
     conditions = []
 
     # Firewall rules
@@ -62,14 +70,36 @@ def FirewallDenyRules (solver, fw, hosts, rules):
     solver.add(z3.ForAll([eh, p], z3.Implies(send(fw, eh, p),
                 z3.Not(z3.Or(conditions)))))
 
-def WebProxyRules (solver, proxy, hosts):
+def WebProxyRules (solver, proxy, adj):
     p = z3.Const('p', packet)
+    p2 = z3.Const('p2', packet)
     eh = z3.Const('temp_eh', endhost)
+    eh2 = z3.Const('temp_eh2', endhost)
+    adjacency_constraint = z3.Or(map(lambda n: eh == n, adj))
     # This is just about connectivity
-    for host in hosts:
-        solver.add(z3.ForAll([eh, p], z3.Implies(send(host, eh, p),\
-                                    eh == proxy)))
-    
+    solver.add(z3.ForAll([eh, p], z3.Implies(recv(eh, proxy, p),\
+                            adjacency_constraint)))
+    solver.add(z3.ForAll([eh, p], z3.Implies(send(proxy, eh, p),\
+                            adjacency_constraint)))
+    solver.add(z3.ForAll([eh, p], z3.Implies(send(proxy, eh, p), address_in_host(proxy, packet.src(p)))))
+    solver.add(z3.ForAll([eh, p], z3.Implies(send(proxy, eh, p), z3.Exists([p2, eh2], 
+                         z3.And(recv(eh2, proxy, p2),
+                             z3.And(packet.origin(p2) == packet.origin(p),
+                                    packet.dest(p2) == packet.dest(p)))))))
+
+
+def EndHostRules (solver, hosts, adj):
+    eh = z3.Const('eh', endhost)
+    adjacency_constraint = z3.Or(map(lambda n: eh == n, adj))
+    for h in hosts:
+        solver.add(z3.ForAll([eh, p], z3.Implies(recv(eh, h, p), address_in_host(h, packet.dest(p)))))
+        solver.add(z3.ForAll([eh, p], z3.Implies(send(h, eh, p), address_in_host(h, packet.src(p)))))
+        solver.add(z3.ForAll([eh, p], z3.Implies(send(h, eh, p), packet.origin(p) == h)))
+        solver.add(z3.ForAll([eh, p], z3.Implies(recv(eh, h, p),\
+                                    adjacency_constraint)))
+        solver.add(z3.ForAll([eh, p], z3.Implies(send(h, eh, p),\
+                                    adjacency_constraint)))
+
 solver = z3.Solver()
 base_conditions = GetBaseConditions ()
 for i in xrange(0, len(base_conditions)):
@@ -87,14 +117,19 @@ solver.add(z3.ForAll([addr], address_in_host(b, addr) == (addr == adb)))
 solver.add(z3.ForAll([addr], address_in_host(c, addr) == (addr == adc)))
 solver.add(z3.ForAll([addr], address_in_host(d, addr) == (addr == add)))
 p = z3.Const('p', packet)
-for h in [a,b,c,d]:
-    solver.add(z3.ForAll([eh, p], z3.Implies(recv(eh, h, p), address_in_host(h, packet.dest(p)))))
-    solver.add(z3.ForAll([eh, p], z3.Implies(send(h, eh, p), address_in_host(h, packet.src(p)))))
-FirewallDenyRules(solver, fw_eh, [a, b, c, d], [(ada, adb), (adc, add)])
-solver.add(z3.Exists([eh], recv(eh, b, p)))
-solver.add(packet.origin(p) == c)
+
+EndHostRules(solver, [a,b], [proxy])
+EndHostRules(solver, [c,d], [fw_eh])
+
+#FirewallDenyRules(solver, fw_eh, [c,d,proxy], [(ada, adc), (adb, add)])
+#WebProxyRules(solver, proxy, [a,b,fw_eh])
+FirewallDenyRules(solver, fw_eh, [a, b, proxy], [(ada, adc), (adb, add)])
+WebProxyRules(solver, proxy, [c, d, fw_eh])
+
+solver.add(z3.Exists([eh], recv(eh, c, p)))
+solver.add(packet.origin(p) == a)
 p2 = z3.Const('p2', packet)
-solver.add(z3.Exists([eh], recv(eh, c, p2)))
+solver.add(z3.Exists([eh], recv(eh, d, p2)))
 solver.add(packet.origin(p2) == b)
 print solver
 print "===================================================================================="
@@ -102,5 +137,5 @@ result = solver.check ()
 print result
 if result == z3.sat:
     model = solver.model()
-    print model
+    print model.sexpr()
 
