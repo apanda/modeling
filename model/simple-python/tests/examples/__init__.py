9b747bf2436c653451e0e9aad3ec01653a904047
Aurojit Panda <apanda@cs.berkeley.edu>
2013-12-02 10:34:17 -0800

    Reorganize source tree

--- model/simple-python/tests/examples/__init__.py
+++ model/simple-python/tests/examples/__init__.py
@@ -1,5 +1,5 @@
 import os, sys
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'mcnet'))
 from twolearningfw import *
 from withProxySat import *
 from withoutProxyAclFw import *
