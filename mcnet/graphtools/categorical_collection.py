ed1c5521005f237aaffafdd984e471b966d8d19f
Aurojit Panda <apanda@cs.berkeley.edu>
2013-12-13 17:48:13 -0800

    Add more comment

--- mcnet/graphtools/categorical_collection.py
+++ mcnet/graphtools/categorical_collection.py
@@ -5,6 +5,10 @@ class CategoricalCollection(UserList):
     """A categorical collection is something that I find hard to believe that Python does not have. It merely wraps up a
     list so function calls get mapped to everything"""
     def __getattr__ (self, attr):
+        """ 
+        Return either of:
+          a. A list of values one from each object.
+          b. A wrapped function which calls the function on all list elements"""
         # First we make sure that the attribute actually exists for all
         if any(map(lambda o: not hasattr(o, attr), self.data)):
             raise AttributeError
--- mcnet/graphtools/factories.py
+++ mcnet/graphtools/factories.py
@@ -3,8 +3,15 @@ from components import LoadBalancer, \
                        AclFirewall, \
                        LearningFirewall, \
                        WebProxy
-""" Some factories for instantiating elements in a nicer way"""
+""" 
+Some factories for instantiating elements in a nicer way. Used when
+creating graphs of elements.
+"""
 def LoadBalancerFactory (saddr, servers):
+    """
+    Factory for load balancer. Load balancers are special in the sense that
+    they require initialization after the entire graph is set up.
+    """
     def InitLoadBalancer (graph, lb):
         servers = map(lambda s: graph[s], servers)
         lb.AddServers (servers)
@@ -15,21 +22,33 @@ def LoadBalancerFactory (saddr, servers):
     return CreateLoadBalancer
 
 def EndHostFactory ():
+    """
+    Factory for endhosts.
+    """
     def CreateEndHost (graph, node):
         return EndHost(node, graph.Network, graph.Context) 
     return CreateEndHost
 
 def AclFirewallFactory ():
+    """
+    Factory for simple ACL based firewalls.
+    """
     def CreateAclFirewall (graph, node):
         return AclFirewall(node, graph.Network, graph.Context) 
     return CreateAclFirewall
 
 def LearningFirewallFactory ():
+    """
+    Factory for Learning Firewalls.
+    """
     def CreateLearningFirewall (graph, node):
         return LearningFirewall(node, graph.Network, graph.Context) 
     return CreateLearningFirewall
 
 def WebProxyFactory ():
+    """
+    Factory for Web Proxies.
+    """
     def CreateWebProxy (graph, node):
         return WebProxy(node, graph.Network, graph.Context) 
     return CreateWebProxy
