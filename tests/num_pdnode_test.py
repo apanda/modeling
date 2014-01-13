f3c0f182039e831ab0c4d3549f1a19fe6c8e63e1
Aurojit Panda <apanda@cs.berkeley.edu>
2014-01-12 18:14:40 -0800

    Some more tweaking

--- tests/num_pdnode_test.py
+++ tests/num_pdnode_test.py
@@ -7,12 +7,12 @@ import mcnet.components as components
 def ResetZ3 ():
     z3._main_ctx = None
     z3.main_ctx()
-    #z3.set_param('auto_config', False)
-    #z3.set_param('smt.mbqi', True)
-    #z3.set_param('model.compact', True)
-    #z3.set_param('smt.pull_nested_quantifiers', True)
-    #z3.set_param('smt.mbqi.max_iterations', 10000)
-
+    z3.set_param('auto_config', False)
+    z3.set_param('smt.mbqi', True)
+    z3.set_param('model.compact', True)
+    z3.set_param('smt.pull_nested_quantifiers', True)
+    z3.set_param('smt.mbqi.max_iterations', 10000)
+bad_in_row = 0
 for it in xrange(0, 100):
     for sz in xrange(1, 1000000):
         ResetZ3()
@@ -21,8 +21,15 @@ for it in xrange(0, 100):
         # Set timeout to some largish number
         obj.check.solver.set(timeout=10000000)
         ret = obj.check.CheckIsolationProperty(obj.e_0, obj.e_1)
-        assert z3.sat == ret.result, \
-                "Nothing stopping this"
+        result = 'bad'
+        if z3.sat == ret.result, \
+            result = 'good'
+            bad_in_row = 0
+        else:
+            bad_in_row += 1
         stop = time.time()
-        print "%d %f"%(sz, stop - start)
+        print "%d %f %s"%(sz, stop - start, result)
+        assert bad_in_row <= 5, \
+                "Too many failures"
+            
 
