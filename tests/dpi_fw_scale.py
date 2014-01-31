fcc4b39466b83df76ca6b37146d9dfa75eb612c5
Aurojit Panda <apanda@cs.berkeley.edu>
2014-01-30 23:08:26 -0800

    Some variation

--- tests/dpi_fw_scale.py
+++ tests/dpi_fw_scale.py
@@ -11,19 +11,19 @@ if len(sys.argv) < 3:
 sz = int(sys.argv[1])
 it = int(sys.argv[2])
 for i in xrange(it):
-    ReseedZ3()
-    pobj = dpiFwRono(sz)
-    print "Done building object"
-    start = time.time()
-    ret = pobj.check.CheckIsolatedIf(pobj.dpi_policy.packetDPIPredicate(pobj.ctx), pobj.a, pobj.b)
-    stop = time.time()
-    time0 = stop - start
-    assert ret.result == z3.sat
+    #ReseedZ3()
+    #pobj = dpiFwRono(sz)
+    #print "Done building object"
+    #start = time.time()
+    #ret = pobj.check.CheckIsolatedIf(pobj.dpi_policy.packetDPIPredicate(pobj.ctx), pobj.a, pobj.b)
+    #stop = time.time()
+    #time0 = stop - start
+    #assert ret.result == z3.sat
     ReseedZ3()
     pobj = dpiFwRono(sz)
     start = time.time()
     ret2 = pobj.check.CheckIsolationProperty(pobj.a, pobj.b)
     stop = time.time()
     assert ret2.result == z3.sat
-    print >>sys.stderr, "%d %f %f"%(i, time0, stop - start)
+    print >>sys.stderr, "%d %f %f"%(i, stop - start)
 
