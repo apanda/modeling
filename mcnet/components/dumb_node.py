228223a427081fd8bd67daa03a683d9fc8101922
Aurojit Panda <apanda@cs.berkeley.edu>
2014-02-03 10:08:15 -0800

    New rules for dumb nodes

--- mcnet/components/dumb_node.py
+++ mcnet/components/dumb_node.py
@@ -17,11 +17,24 @@ class DumbNode (NetworkObject):
         n3 = z3.Const('__nodeRules_Node3', self.ctx.node)
         p = z3.Const('__nodeRules_Packet', self.ctx.packet)
         p2 = z3.Const('__nodeRules_Packet2', self.ctx.packet)
+
+        # Appending what sort of packet are sent normally
         solver.append(z3.ForAll([n, p], \
                 z3.Implies(self.ctx.send(self.node, n, p), \
                     z3.Or(self.ctx.hostHasAddr(self.node, self.ctx.packet.src(p)), \
                        z3.Exists([n2, p2], z3.And(self.ctx.recv(n2, self.node, p2), \
-                                                  self.ctx.PacketsHeadersEqual(p, p2)))))))
+                                                       self.ctx.PacketsHeadersEqual(p, p2), \
+                                                       self.ctx.etime(self.node, p, self.ctx.send_event) > \
+                                                        self.ctx.etime(self.node, p2, self.ctx.recv_event)))))))
+        # Appending what sort of packet are sent normally
+        solver.append(z3.ForAll([n, p], \
+                z3.Implies(self.ctx.send(self.node, n, p), \
+                    z3.Or(self.ctx.packet.origin(p) == self.node, \
+                          z3.Exists([n2, p2], z3.And(self.ctx.recv(n2, self.node, p2), \
+                                                   self.ctx.packet.origin(p2) ==\
+                                                        self.ctx.packet.origin(p),
+                                                    self.ctx.etime(self.node, p, self.ctx.send_event) > \
+                                                     self.ctx.etime(self.node, p2, self.ctx.recv_event)))))))
         #solver.append(z3.ForAll([n, n2, p], \
                 #z3.Implies(z3.And(self.ctx.send(n, n2, p), \
                         #self.ctx.hostHasAddr(self.node, self.ctx.packet.src(p))), \
