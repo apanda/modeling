cfb132d4a1a837a99835edf92ac7bab1f51dc356
Aurojit Panda <apanda@cs.berkeley.edu>
2014-02-03 22:33:35 -0800

    Add a large example

--- test_expansion/__init__.py
+++ test_expansion/__init__.py
@@ -3,3 +3,4 @@ sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 
 from full_model import FullModel
 from part_model_01 import PartModel01
 from subgraph_model import PartSubgraph 
+from large_subgraph_model import Part02Subgraph
---
+++ test_expansion/large_subgraph_model.py
@@ -0,0 +1,56 @@
+from mcnet.components import SubgraphProblem, EndHost, AclFirewall, Context, Network, LSRRRouter, LSRROption
+def Part02Subgraph (nlsrr):
+    # Initialization
+    lsrr_boxes = ['l%d'%(l) for l in xrange(nlsrr)]
+    lsrr_address = ['ip_%s'%(l) for l in lsrr_boxes]
+    firewalls = ['f0', 'f1', 'f2', 'f3', 'f4']
+    f_address = ['ip_%s'%(f) for f in firewalls]
+    endhosts = ['e0', 'e1', 'e2', 'e3', 'e4']
+    e_address = ['ip_%s'%(e) for e in endhosts]
+
+    nodes = list(lsrr_boxes)
+    addresses = list(lsrr_address)
+
+    nodes.extend(firewalls)
+    addresses.extend(f_address)
+
+    nodes.extend(endhosts)
+    addresses.extend(e_address)
+
+    ctx = Context(nodes, addresses)
+    net = Network(ctx)
+
+    net.setAddressMappings([(getattr(ctx, n), getattr(ctx, a)) \
+                           for n, a in zip(nodes, addresses)])
+
+    constructed = {}
+
+    for eh in endhosts:
+        constructed[eh] = EndHost(getattr(ctx, eh), net, ctx)
+
+    for fidx in xrange(len(firewalls)):
+        f = firewalls[fidx]
+        constructed[f] = AclFirewall(getattr(ctx, f), net, ctx)
+        ead = getattr(ctx, e_address[fidx])
+        constructed[f].AddAcls([(ead, eother) for eother in xrange(len(endhosts)) if eother != fidx])
+
+    # Register something that tells us about LSRR
+    ip_lsr_field = LSRROption ('ip_lsr', ctx)
+    ctx.AddPolicy (ip_lsr_field)
+
+    for lsrr in lsrr_boxes:
+        constructed[lsrr] = LSRRRouter (getattr(ctx, lsrr), ip_lsr_field, net, ctx)
+
+    base_routes = [x for x in zip(map(lambda a:getattr(ctx, a), e_address), \
+                                  map(lambda f:constructed[f], firewalls))]
+    base_routes.extend([x for x in zip(map(lambda a:getattr(ctx, a), lsrr_address), \
+                                       map(lambda n:constructed[n], lsrr_boxes))])
+    routing = {n: list(base_routes) for n in nodes}
+
+    prob = SubgraphProblem(ctx)
+    prob.network = net
+    prob.origin = constructed['e0']
+    prob.target = constructed['e1']
+    prob.node_map = constructed
+    prob.tfunctions = routing
+    return prob
