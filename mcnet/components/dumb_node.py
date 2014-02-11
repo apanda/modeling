bf43ac65597cf59ecc86140d5a43b392ef6563c4
Aurojit Panda <apanda@cs.berkeley.edu>
2014-02-10 21:46:55 -0800

    Break things again

--- mcnet/components/dumb_node.py
+++ mcnet/components/dumb_node.py
@@ -18,14 +18,14 @@ class DumbNode (NetworkObject):
         p = z3.Const('__nodeRules_Packet', self.ctx.packet)
         p2 = z3.Const('__nodeRules_Packet2', self.ctx.packet)
         ## When packets are sent they take their sending address from one of two places
-        self.constraints.append(z3.ForAll([n, p], \
+        solver.append(z3.ForAll([n, p], \
                 z3.Implies(self.ctx.send(self.node, n, p), \
                     z3.Or(self.ctx.hostHasAddr(self.node, self.ctx.packet.src(p)), \
                        z3.Exists([n2, p2], z3.And(self.ctx.recv(n2, self.node, p2), \
                                                        self.ctx.PacketsHeadersEqual(p, p2), \
                                                        self.ctx.etime(self.node, p, self.ctx.send_event) > \
                                                         self.ctx.etime(self.node, p2, self.ctx.recv_event)))))))
-        self.constraints.append(z3.ForAll([n, p], \
+        solver.append(z3.ForAll([n, p], \
                 z3.Implies(self.ctx.send(self.node, n2, p), \
                         z3.Or(self.ctx.packet.origin(p) == self.node, \
                               z3.Exists([n2, p2], \
