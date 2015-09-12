0ee19c2415644854759105c674cc97c0cb9b629c
Aurojit <apanda@cs.berkeley.edu>
2015-09-12 15:01:26 -0700

    Some more slice stuff

---
+++ tests/examples/amazontest.py
@@ -0,0 +1,111 @@
+import components
+from itertools import repeat
+def AmznPFWMultiTenantUnattach (tenant, external, internal):
+  total_per_tenant = external + internal
+  total = tenant * total_per_tenant
+  hosts = ['h%d'%i for i in xrange(total)]
+  host_addresses = ['ip_%s'%(i) for i in hosts]
+  firewalls = ['f%d'%i for i in xrange(total)]
+  fw_addresses = ['ip_%s'%(i) for i in firewalls]
+  outside = []
+  out_addresses = ['ip_%s'%i for i in outside]
+  fabric = ['fab']
+  fabric_address = ['ip_fab']
+
+  
+  nodes = list(hosts)
+  nodes.extend(firewalls)
+  nodes.extend(outside)
+  nodes.extend(fabric)
+  
+  addresses = list(host_addresses)
+  addresses.extend(fw_addresses)
+  addresses.extend(out_addresses)
+  addresses.extend(fabric_address)
+
+  # Network construction
+  ctx = components.Context(nodes, addresses)
+  net = components.Network(ctx)
+  address_mappings = []
+
+  priv_groups = ['priv_%d'%t for t in xrange(tenant)]
+  pub_groups = ['pub_%d'%t for t in xrange(tenant)]
+  sec_groups = priv_groups
+  sec_groups.extend(pub_groups)
+  sgpolicy = components.SecurityGroups("sgpolicy", sec_groups, ctx, net) 
+
+  fabric_concrete = []
+  for (fab, fad) in zip(fabric, fabric_address):
+    fabric_concrete.append(components.Fabric(getattr(ctx, fab), net, ctx))
+    address_mappings.append((fabric_concrete[-1], getattr(ctx, fad)))
+  
+  assert(len(fabric_concrete) == 1)
+
+  host_concrete = []
+  for (h, a) in zip(hosts, host_addresses):
+    host_concrete.append(components.EndHost(getattr(ctx, h), net, ctx))
+    address_mappings.append((host_concrete[-1], getattr(ctx, a)))
+  
+  firewall_concrete = []
+  for (fw, a) in zip(firewalls, fw_addresses):
+    firewall_concrete.append(components.AmznDenyNoLearnFirewall(getattr(ctx, fw), net, ctx, sgpolicy))
+    address_mappings.append((firewall_concrete[-1], getattr(ctx, a)))
+  
+  outside_concrete = []
+  for (out, a) in zip(outside, out_addresses):
+    outside_concrete.append(components.EndHost(getattr(ctx, out), net, ctx))
+    address_mappings.append((outside_concrete[-1], getattr(ctx, a)))
+  net.setAddressMappings(address_mappings)
+
+  # Routing table
+  for (h, f) in zip(host_concrete, firewall_concrete):
+    net.SetIsolationConstraint(h, [f])
+    net.SetIsolationConstraint(f, [h, fabric_concrete[0]])
+
+  #for (h, f) in firewall_concrete:
+    #net.SetGateway(f, fabric_concrete[0])
+
+  routing_table = []
+  for (a, f) in zip(host_addresses, firewall_concrete):
+    routing_table.append((getattr(ctx, a), f))
+  fabric_concrete[0].AddRoutes(routing_table)
+
+  # Policy
+  # first internal hosts for each tenant are internals, rest are external
+  for t in xrange(tenant):
+    tbegin = t * total_per_tenant
+    assignment = []
+    for a in host_addresses[tbegin : tbegin + internal]:
+      assignment.append((getattr(ctx, a), 'priv_%d'%(t)))
+    for a in host_addresses[tbegin + internal : tbegin + total_per_tenant]:
+      assignment.append((getattr(ctx, a), 'pub_%d'%(t)))
+    sgpolicy.addAddressToGroup(assignment)
+
+  for t in xrange(tenant):
+    # The firewall is default deny, so we specify all the things that we allow in here.
+    tbegin = t * total_per_tenant
+    policies = []
+    # Private to private
+    policies.append(('priv_%d'%(t), 'priv_%d'%(t)))
+    ## Public to private
+    #policies.append(('pub_%d'%(t), 'priv_%d'%(t)))
+    # Private to any
+    policies.append(('priv_%d'%(t), True))
+    # Public to any
+    #policies.append(('pub_%d'%(t), True))
+    # Any to public
+    policies.append((True, 'pub_%d'%(t)))
+    for fw in firewall_concrete[tbegin:tbegin + total_per_tenant]:
+      fw.AddPolicies(policies)
+  net.Attach(*fabric_concrete)
+  class TennantRet (object):
+    def __init__ (self):
+      self.outside = outside_concrete
+      self.hosts = host_concrete
+      self.firewalls = firewall_concrete
+      self.net = net
+      self.ctx = ctx
+      self.fabric = fabric_concrete[0]
+      self.checker = components.PropertyChecker(ctx, net)
+      self.sgroup = sgpolicy
+  return TennantRet()
---
+++ tests/fattree_pfw_test_slice.py
@@ -0,0 +1,121 @@
+from examples import PFWMultiTenantUnattach
+import z3
+import time
+import random
+import sys
+import argparse
+
+def ResetZ3 (seed):
+    z3._main_ctx = None
+    z3.main_ctx()
+    z3.set_param('smt.random_seed', seed)
+
+def Rono(tenants, internal, external, seed, samples):
+    # Check all internal unreachable from each other
+    # Since we are just using slices here
+    internal = 1
+    external = 1
+    tenants = 2
+    total_per_tenant = internal + external
+    int_checks = 0
+    int_time = 0.0
+    times = []
+    for i in xrange(internal):
+        if int_checks >= samples:
+            break
+        for t in xrange(1, tenants):
+            ResetZ3(seed)
+            topo = PFWMultiTenantUnattach(tenants, external, internal)
+            start = time.time()
+            topo.net.Attach(topo.hosts[i], topo.hosts[i + (t * total_per_tenant)], \
+                          topo.firewalls[i], topo.firewalls[i + (t * total_per_tenant)])
+            result = topo.checker.CheckIsolationFlowProperty(topo.hosts[i], topo.hosts[i + (t * total_per_tenant)])
+            assert result.result == z3.unsat
+            stop = time.time()
+            int_time += (stop - start)
+            int_checks += 1
+            times.append(stop - start)
+            if int_checks >= samples:
+                break
+    int_average = int_time / float(int_checks)
+    # Check externals from other tenants cannot reach internal 0
+    ext_int_time = 0.0
+    ext_int_checks = 0
+    for i in xrange(external):
+        if ext_int_checks >= samples:
+            break
+        for t in xrange(1, tenants):
+            ResetZ3(seed)
+            topo = PFWMultiTenantUnattach(tenants, external, internal)
+            start = time.time()
+            topo.net.Attach(topo.hosts[i + (t * total_per_tenant) + internal],\
+                    topo.hosts[0],\
+                    topo.firewalls[i + (t * total_per_tenant) + internal],\
+                    topo.firewalls[0])
+            result = topo.checker.CheckIsolationFlowProperty(topo.hosts[i + (t * total_per_tenant) + internal],\
+                    topo.hosts[0])
+            assert result.result == z3.unsat
+            stop = time.time()
+            ext_int_time += (stop - start)
+            ext_int_checks += 1
+            times.append(stop - start)
+            if ext_int_checks >= samples:
+                break
+    ext_int_average = ext_int_time / float(ext_int_checks)
+    int_ext_checks = 0
+    int_ext_time = 0.0
+    # Check externals from other tenants are reachable from internal 0
+    for i in xrange(external):
+        if int_ext_checks >= samples:
+            break
+        for t in xrange(1, tenants):
+            ResetZ3(seed)
+            topo = PFWMultiTenantUnattach(tenants, external, internal)
+            start = time.time()
+            topo.net.Attach(topo.hosts[0],\
+                    topo.hosts[i + (t * total_per_tenant) + internal],\
+                    topo.firewalls[0],\
+                    topo.firewalls[i + (t * total_per_tenant) + internal])
+            result = topo.checker.CheckIsolationFlowProperty(topo.hosts[0],\
+                    topo.hosts[i + (t * total_per_tenant) + internal])
+            assert result.result == z3.sat
+            stop = time.time()
+            int_ext_time += (stop - start)
+            int_ext_checks += 1
+            times.append(stop - start)
+            if int_ext_checks >= samples:
+                break
+    int_ext_average = int_ext_time / float(int_ext_checks)
+    return (int_average, ext_int_average, int_ext_average, times)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = 'RONO fat-tree test')
+    parser.add_argument('--iters', type=int, nargs='?', default=10)
+    parser.add_argument('--imin', type=int, nargs='?', default=1)
+    parser.add_argument('--imax', type=int, nargs='?', default=10)
+    parser.add_argument('--emin', type=int, nargs='?', default=1)
+    parser.add_argument('--emax', type=int, nargs='?', default=10)
+    parser.add_argument('--tmin', type=int, nargs='?', default=2)
+    parser.add_argument('--tmax', type=int, nargs='?', default=25)
+    parser.add_argument('--tskip', type=int, nargs='?', default=1)
+    parser.add_argument('--seed', type=int, nargs='?', default=42)
+    parser.add_argument('--samples', type=int, nargs='?', default=5)
+    args = parser.parse_args()
+    iters = args.iters
+    int_min = args.imin
+    ext_min = args.emin
+    tenant_min = args.tmin
+    int_max = args.imax
+    ext_max = args.emax
+    tenant_max = args.tmax
+    tskip = args.tskip
+    seed = args.seed
+    samples = args.samples
+    print "tenant int ext ia eia iea times"
+    for iter in xrange(iters):
+        for tenant in xrange(tenant_min, tenant_max, tskip):
+            for i in xrange(int_min, int_max):
+                for e in xrange(ext_min, ext_max):
+                    (ia, eia, iea, times) = Rono(tenant, i, e, seed, samples)
+                    times = ' '.join(map(str, times))
+                    print "%d %d %d %f %f %f %s"%(tenant, i, e, ia, eia, iea, times)
